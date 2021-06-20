50e0dfa892f27092cc7a845738e0c9e03f3db470
The-Freezen <camo19@ymail.com>
2021-06-20 13:15:26 -0500

    update messaging

--- mongodb_test.py
+++ mongodb_test.py
@@ -4,5 +4,6 @@ from pymongo import MongoClient
 url = "mongodb+srv://admin:admin@cluster0.b9dss.mongodb.net/CSD?retryWrites=true&w=majority"
 client = MongoClient(url)
 db = client.pytech
-
+print("--Pytech Collection List--")
 print(db.list_collection_names())
+input("End of program, press any key to exit... ")
